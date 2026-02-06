c83e3c661dc266edba4d43062c48b8e33835055c
managercoby-spec <managercoby@gmail.com>
2026-02-06 17:23:01 +0200

    Delete src/lib/supabase directory

--- src/lib/supabase/client.ts
+++
@@ -1,7 +0,0 @@
-'use client'
-
-import { createClientComponentClient } from '@supabase/auth-helpers-nextjs'
-import { Database } from '@/types/database.types'
-
-export const supabase = createClientComponentClient<Database>()
-
